f89724742d8bd31e9c8b01f3b021e8ce016a2e59
taariqdb17 <taariqdb17@gmail.com>
2024-03-07 21:49:33 +0000

    Some important notes added in ScatterAndBubblechart R file

--- ScatterAndBubblechart.R
+++ ScatterAndBubblechart.R
@@ -15,3 +15,12 @@ dfbc <- read_excel("D:/R Programing Scripts Practice/R-Script-/Data/Bubblechart.
 View(dfbc)
 bubble_p <- ggplot(dfbc,aes(x=Revenue,y=`Profit %`,size=Profit,col=Country))+geom_point(alpha=0.95)
 bubble_p
+
+#Notes
+#Scatter plot is a graph that uses a grid to present data values, typically involving two variables 
+#within a dataset.
+#Scatter plot help to visualize the relationship between two variables.
+#A bubble chart is a variation of a scatter plot that allows you to represent a third variables
+#through the size of the bubbles.
+#Scatter plot and Bubble charts help to detect the outliers, visulaize the relationship
+#between two variables and also identify trends
